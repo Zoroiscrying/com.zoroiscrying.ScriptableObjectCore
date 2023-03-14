b6cea730414a0c58d3bb417a4b8f813676a81fc4
Zoroiscrying <43092827+Zoroiscrying@users.noreply.github.com>
2023-03-14 01:58:21 +0100

    Initialized

---
+++ Documentation~/Zoroiscrying.ScriptableObjectCore.md
@@ -0,0 +1,5 @@
+Package for utilizing scriptable object for game development in Unity, inspired by Atoms.
+
+Documentation for Unity package manager window goes here.
+Also if images are used in README.md / docs this is a good place to store those as well.
+
---
+++ Runtime/TestRuntime.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace com.zoroiscrying.ScriptableObjectCore
+{
+    public class TestRuntime : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
